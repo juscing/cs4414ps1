e2951a1777cd2bf4e914a9984475b90d911a2b0d
juscing <juscing@gmail.com>
2014-01-26 21:22:09 -0500

    Did problems 4 and 5 styles.html for demo

--- zhttpto.rs
+++ zhttpto.rs
@@ -15,14 +15,15 @@
 use std::io::*;
 use std::io::net::ip::{SocketAddr};
 use std::{str};
+use std::io::buffered::BufferedReader;
 
 static IP: &'static str = "127.0.0.1";
 static PORT:        int = 4414;
+static mut visitor_count: int = 0;
 
 fn main() {
     let addr = from_str::<SocketAddr>(format!("{:s}:{:d}", IP, PORT)).unwrap();
     let mut acceptor = net::tcp::TcpListener::bind(addr).listen();
-    static mut visitor_count: int = 0;
     
     println(format!("Listening on [{:s}] ...", addr.to_str()));
     
@@ -46,9 +47,59 @@ fn main() {
             let request_str = str::from_utf8(buf);
             println(format!("Received request :\n{:s}", request_str));
             
-            unsafe {visitor_count += 1;}
-            let response: ~str = 
-                ~"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+            //Get the first line of the request
+            let fl_items: ~[&str] = request_str.split(' ').collect();
+            let mut path = fl_items[1];
+            
+            path = path.slice_from(1);
+            
+            let ext;
+            if path.len() > 4 {
+	      ext = path.slice_from(path.len() - 4);
+	    } else {
+	      ext = "null"
+	    }
+            
+            let p = Path::new(path);
+            
+            //Remove this
+            //print(path);
+            
+            let mut response: ~str = ~"";
+            
+            if str::eq(&~"html", &ext.into_owned()) {
+            if p.is_file() {
+	      match File::open(&Path::new(path)) {
+	      Some(file) => {
+		let mut reader = BufferedReader::new(file);
+		response = response + "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n";
+		for line in reader.lines() {
+		  response = response + line;
+		}
+		
+	      }
+	      None =>{
+		response = foo();
+	      }
+	    }
+            } else {
+	      response = foo();
+            }
+            } else {
+	      response = foo();
+            }
+            
+            
+            
+            
+            stream.write(response.as_bytes());
+            println!("Connection terminates.");
+        }
+    }
+}
+fn foo() -> ~str {
+	unsafe {visitor_count += 1;}
+	"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
                  <doctype !html><html><head><title>Hello, Rust!</title>
                  <style>body { background-color: #111; color: #FFEEAA }
                         h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
@@ -56,9 +107,5 @@ fn main() {
                  </style></head>
                  <body>
                  <h1>Greetings, Krusty " + unsafe{visitor_count.to_str()} + "!</h1>
-                 </body></html>\r\n";
-            stream.write(response.as_bytes());
-            println!("Connection terminates.");
-        }
-    }
+                 </body></html>\r\n"
 }
